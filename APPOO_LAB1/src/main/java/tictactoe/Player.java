bffe1dea2268b13932408b66c8870fec7a30f050
thirteenmd <andreacojocaru@gmail.com>
2018-02-14 23:43:35 +0200

    added Player.java class

---
+++ APPOO_LAB1/src/main/java/tictactoe/Player.java
@@ -0,0 +1,22 @@
+package tictactoe;
+
+public class Player {
+    private String name;
+    private char mark;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getMark() {
+        return mark;
+    }
+
+    public void setMark(char mark) {
+        this.mark = mark;
+    }
+}
